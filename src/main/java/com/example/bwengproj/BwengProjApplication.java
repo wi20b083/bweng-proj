4deea71fb0c9dbec48bbf3501e3171bcd0110f9e
wi20b083 <wi20b083@technikum-wien.at>
2022-11-22 12:35:45 +0100

    Initial commit

---
+++ src/main/java/com/example/bwengproj/BwengProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.bwengproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwengProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwengProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bwengproj/BwengProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bwengproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwengProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
