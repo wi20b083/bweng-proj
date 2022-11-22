9c39caa2b627547609336e3da0dfdff147642ae5
wi20b083 <wi20b083@technikum-wien.at>
2022-11-22 20:16:22 +0100

    database entities

--- src/main/java/com/example/bwengproj/model/AuctionItem.java
+++ src/main/java/com/example/bwengproj/model/AuctionItem.java
@@ -1,6 +1,30 @@
 package com.example.bwengproj.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class AuctionItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auction_id")
+    private Auction auction;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
     private int amount;
     private int price;
 }
--- src/main/java/com/example/bwengproj/model/Product.java
+++ src/main/java/com/example/bwengproj/model/Product.java
@@ -1,7 +1,25 @@
 package com.example.bwengproj.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
-    private int productID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String imgLink;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "product")
+    private Set<AuctionItem> products;
 }
