a63c74e09117cac0d970fd5dc10be1540bca07af
wi20b083 <wi20b083@technikum-wien.at>
2022-11-22 15:59:22 +0100

    auction-user relationship

--- src/main/java/com/example/bwengproj/model/User.java
+++ src/main/java/com/example/bwengproj/model/User.java
@@ -1,9 +1,33 @@
 package com.example.bwengproj.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(fluent = true, chain = true)
 public class User {
-    private int UserID;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     private UserRole role;
     private String firstName;
     private String lastName;
     private String imgLink;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private Set<Auction> auctions;
+
+
 }
