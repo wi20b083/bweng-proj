68bff98dcded20904679c560c2112adb6b14d35e
wi20b083 <wi20b083@technikum-wien.at>
2022-11-23 15:20:45 +0100

    asdf

--- src/main/java/com/example/bwengproj/controllers/UserController.java
+++ src/main/java/com/example/bwengproj/controllers/UserController.java
@@ -1,47 +1,55 @@
 package com.example.bwengproj.controllers;
 
-import com.example.bwengproj.model.Bid;
+import com.example.bwengproj.model.Auction;
 import com.example.bwengproj.model.User;
-import com.example.bwengproj.services.BidService;
 import com.example.bwengproj.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Set;
 
 @RestController
 public class UserController {
     //@Autowired
     private UserService userService;
 
+
+    // /users
     // Save operation
     @PostMapping("/users")
-    public User saveUser(@Valid @RequestBody User user){
+    public User saveUser(@Valid @RequestBody User user) {
 
         return userService.saveUser(user);
     }
 
     // Read operation
     @GetMapping("/users")
-    public Iterable<User> fetchUserList(){
+    public Iterable<User> fetchUserList() {
         return userService.fetchUserList();
     }
 
+
+    // /users/id
     // Update operation
     @PutMapping("/users/{id}")
-    public User updateUser(@RequestBody User user, @PathVariable("id") long userId){
+    public User updateUser(@RequestBody User user, @PathVariable("id") long userId) {
         return userService.updateUser(user, userId);
     }
 
     // Delete operation
     @DeleteMapping("/users/{id}")
-    public String deleteUserById(@PathVariable("id") long userId){
+    public String deleteUserById(@PathVariable("id") long userId) {
         userService.deleteUserById(userId);
         return "Deleted Successfully";
     }
 
     // get by id
     @GetMapping("/users/{id}")
-    public User fetchUserByID(@PathVariable("id") long userId){
+    public User fetchUserByID(@PathVariable("id") long userId) {
         return userService.fetchUserById(userId);
     }
+
+    // /users/id/data
+    @GetMapping("/users/{id}/auctions")
+    public Set<Auction>
 }
